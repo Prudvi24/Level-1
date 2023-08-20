38a9146b2aae9553f4598e85c7b741b13afdf902
Prudvi24 <deti.prudvi@gmail.com>
2023-08-20 14:06:38 +0530

    post increment and assignment operator overloading

--- OOPS/OOPS_part2/7_operator_loading_3.cpp
+++ OOPS/OOPS_part2/7_operator_loading_3.cpp
@@ -20,7 +20,9 @@ In 7_operator_overloading_2.cpp we will discuss
 4. pre incremenet operator
 	4.1 ==> ++f1
 	4.2 ==> f3 = ++f1
-	4.3 ==> f3 = ++(++f2)  Nested pre-increment in this program
+	4.3 ==> f3 = ++(++f2)  Nested pre-increment
+
+5. post-increment (in this program)
 */
 
 class Fraction{
@@ -92,7 +94,30 @@ public:
 		this->simplify();
 		return *this;   // return the current object.
 	}
-	 
+
+	// "++" post-increment operator overloading
+	Fraction operator++(int){                    // nesting is not allowed in post-increment
+		Fraction fnew(this->numerator, this->denominator);
+		this->numerator += this->denominator;
+		this->simplify();
+		fnew.simplify();
+		return fnew;
+	}
+
+	// "+=" operator overloading (nesting is allowed)
+	Fraction& operator+= (const Fraction &f){
+		int lcm = this->denominator * f.denominator;
+		int x = lcm / denominator;
+		int y = lcm / f.denominator;
+
+		int num = x*numerator + (y*f.numerator);
+
+		numerator = num;
+		denominator = lcm;
+		simplify();
+
+		return *this;
+	}
 };
 
 int main(){
@@ -101,53 +126,23 @@ int main(){
 	f1.print();
 	cout<<"-----------------------"<<endl<<endl;
 
-	cout<<"Pre-incremenet of f1: "<<endl;
-	Fraction f3 = ++f1;
+	cout<<"post increment of f1: "<<endl;
+	Fraction f3 = f1++;
+	cout<<"Fraction f1"<<endl;
 	f1.print();
-	cout<<"-----------------------"<<endl<<endl;
-
-	cout<<"Fraction 3: "<<endl;
+	cout<<endl;
+	cout<<"Fraction f3"<<endl;
 	f3.print();
+    cout<<"-----------------------"<<endl<<endl;
 
-	// We can pre-increment twice but it wont return the correct result
 	cout<<"Fraction 2: "<<endl;
 	Fraction f2(8,4);
 	f2.print();
 	cout<<"-----------------------"<<endl<<endl;
 
-	cout<<"pre-increment twice on f2"<<endl;
-	Fraction f4 = ++(++f2);
-	cout<<"Fraction f2 is"<<endl;
-	f2.print();cout<<endl;
-	cout<<"Fraction f4 is"<<endl;
-	f4.print();   // printing 4/1 ("correct answer")
-
-	/*
-
-	In the previous file / program
-
-	Fraction operator++(){                           // we are recieving in the new block
-		this->numerator += this->denominator;
-		this->simplify();
-		return *this;   // return the current object.
-	}
-	
-	when we do (++f2) it update f2 and return an answer in new block 
-	
-	(means creating creating a new block and return new block address / temporary block after 1st pre-increment);
-	
-	so when we capture in new fraction f4 we could get the result but it wont'get update in f2 (second pre-increment).
-
-	To resolve this, "anyhow we must return the address/ reference of f2 after the first increment"
-	Now, even if we use nested pre-increment we it will work.
-
-	Fraction& operator++(){                        // we are recieving in the same block because we have returning using reference here
-		this->numerator += this->denominator;
-		this->simplify();
-		return *this;   // return the current object.
-	}
-	*/
-
+	cout<<"+= operator on f2"<<endl;
+	(f2 += f1) += f3;
+	f2.print();
 	return 0;
 }
 /*
@@ -156,21 +151,19 @@ Fraction 1:
 Fraction is: 10 / 2
 -----------------------
 
-Pre-incremenet of f1: 
+post increment of f1: 
+Fraction f1
 Fraction is: 6 / 1
+
+Fraction f3
+Fraction is: 5 / 1
 -----------------------
 
-Fraction 3: 
-Fraction is: 6 / 1
 Fraction 2: 
 Fraction is: 8 / 4
 -----------------------
 
-pre-increment twice on f2
-Fraction f2 is
-Fraction is: 4 / 1
-
-Fraction f4 is
-Fraction is: 4 / 1
++= operator on f2
+Fraction is: 13 / 1
 
 */
