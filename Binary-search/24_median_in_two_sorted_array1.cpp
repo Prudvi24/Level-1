83074d2e45d9e04decf3fabbe4df218eb040c4ea
Prudvi24 <deti.prudvi@gmail.com>
2023-09-06 14:23:46 +0530

    bug cleared

--- Binary-search/24_median_in_two_sorted_array1.cpp
+++ Binary-search/24_median_in_two_sorted_array1.cpp
@@ -72,11 +72,11 @@ int main(){
 	int n,m;
 	cin>>n>>m;
 	vector<int>a(n,0);
-	vector<int>b(n,0);
+	vector<int>b(m,0);
 	for(int i=0;i<n;i++){
 		cin>>a[i];
 	}
-	for(int i=0;i<n;i++){
+	for(int i=0;i<m;i++){
 		cin>>b[i];
 	}
 	double ans = median(a,b);
