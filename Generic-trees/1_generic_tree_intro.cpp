2b7ecb5baf1649fa71d2bc3675f08bb36d18884e
Prudvi24 <deti.prudvi@gmail.com>
2024-05-05 22:35:24 +0530

    generic tree introduction

---
+++ Generic-trees/1_generic_tree_intro.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+public:
+	int data;
+	vector<Node*>children;
+};
+
+Node* BuildGenericTree(vector<int>&arr){
+	Node *root = nullptr;
+	stack<Node*>st;
+	for(int i=0;i<arr.size();i++){
+		if(arr[i]==-1){
+			st.pop();
+		}
+		else{
+			Node *newNode = new Node();
+			newNode->data = arr[i];
+			//if the stack has node
+			if(st.size()>0){
+				Node *curr = st.top();
+				curr->children.emplace_back(newNode);
+			}
+			else{
+				root = newNode;
+			}
+			st.push(newNode);
+		}
+	}
+    return root;
+}
+
+void display(Node *root){
+	cout<<root->data<<endl;
+	for(int i=0; i<root->children.size(); i++){
+		display(root->children[i]);
+	}
+}
+
+int main(){
+	vector<int>arr = {10,20,50,-1,60,-1,-1,30, 70, -1, 80, 110, 
+					 -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, 
+					 -1, -1};
+	Node *root = BuildGenericTree(arr);
+	display(root);
+	return 0;
+}
